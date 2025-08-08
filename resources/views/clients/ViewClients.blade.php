6cf97a83ff4ad6146b7503937ed08f0a0327240e
Redjyll Brint <redjyll_ondo@dmc.edu.ph>
2025-08-08 11:31:34 +0800

    ui

--- resources/views/clients/ViewClients.blade.php
+++ resources/views/clients/ViewClients.blade.php
@@ -45,6 +45,7 @@
                             opacity: 0;
                             transition: opacity 0.3s ease;
                             z-index: 0;
+                            margin-bottom: 1em;
                         }
 
                         .hover-layer:hover::before {
